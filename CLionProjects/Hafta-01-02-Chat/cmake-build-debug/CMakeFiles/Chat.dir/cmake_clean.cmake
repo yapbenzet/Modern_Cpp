e0ed02672e2cbf8d6c1d42dd3928d4f9d7571fa2
Ateş Arkuran <ates.arkuran@gmail.com>
2020-03-02 17:45:07 +0300

    Add files via upload

---
+++ CLionProjects/Hafta-01-02-Chat/cmake-build-debug/CMakeFiles/Chat.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chat.dir/chat_sinif.cpp.o"
+  "CMakeFiles/Chat.dir/main.cpp.o"
+  "Chat"
+  "Chat.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
