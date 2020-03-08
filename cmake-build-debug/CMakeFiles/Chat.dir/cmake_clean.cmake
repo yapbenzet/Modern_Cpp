439937d89a382d0055e582eff1b52ca2e5091a76
AtesArkuran <ates.arkuran@gmail.com>
2020-03-08 16:15:46 +0300

    CMakeLists.txt oluşturuldu

---
+++ cmake-build-debug/CMakeFiles/Chat.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chat.dir/Hafta-01/02-Chat/chat_sinif.cpp.o"
+  "CMakeFiles/Chat.dir/Hafta-01/02-Chat/main.cpp.o"
+  "Chat"
+  "Chat.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
